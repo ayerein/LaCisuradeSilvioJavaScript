b0b9b29b4316840f116581be90b0f4e6e56b8c68
coseres <coseresmauricio@gmail.com>
2022-02-09 04:44:26 -0300

    Desafio: incorporar eventos

--- simulador.js
+++ simulador.js
@@ -1,4 +1,150 @@
-let cantidad = 0
+const contenedor = document.getElementById("contenedor")
+const contenedorCat = document.getElementById("contenedorCat")
+
+const catRopa = document.getElementById("ropa")
+const catAccesorios = document.getElementById("accesorios")
+
+const btncarrito = document.getElementById("btnCarrito")
+
+
+let botonAgr = document.getElementsByClassName('btnComprar')
+
+let carrito = []
+let precioFinal = 0
+
+
+
+function mostrarProductos(array){
+    contenedor.innerHTML='';
+    array.forEach(producto => {
+        
+        let div = document.createElement('div')
+        div.className = 'producto'
+        div.innerHTML += `<div class="card">
+                            <div class="imagen">
+                                <img src="img/img.jpg" alt="">
+                            </div>
+                            <div class="descripcion">
+                                <h4>${producto.nombre} ${producto.talle}</h4>
+                                <button id="${producto.id}" class="btnComprar" >comprar</button>
+                            </div>  
+                        </div>
+                        `
+        contenedor.appendChild(div)
+
+    });
+}
+
+function mostrarCarrito(){
+    contenedor.innerHTML='';
+    
+    let divCarrito = document.createElement('div')
+    divCarrito.className = 'carrito'
+    divCarrito.innerHTML += `
+    <p>El precio total del carrito es de $${precioFinal}</p>
+    `
+    contenedor.appendChild(divCarrito)
+}
+
+
+
+
+catRopa.addEventListener('click', ()=>{
+    console.log(catRopa.id)
+    contenedorCat.innerHTML='';
+    if(catRopa.id == 'ropa'){
+        mostrarProductos(stockProductos.filter(el => el.categoria == catRopa.id))
+    }
+    for (const boton of botonAgr){
+        boton.addEventListener('click', ()=>{
+            let producto = stockProductos.find(item => item.id == boton.id)
+            console.log(`agregaste al carrito ${producto.nombre} ${producto.talle}`)
+            carrito.push(producto)
+            precioFinal += producto.precio
+        })
+    }
+})
+
+catAccesorios.addEventListener('click', ()=>{
+    console.log(catAccesorios.id)
+    contenedorCat.innerHTML='';
+    if(catAccesorios.id == 'accesorios'){
+        mostrarProductos(stockProductos.filter(el => el.categoria == catAccesorios.id))
+    }
+    for (const boton of botonAgr){
+        boton.addEventListener('click', ()=>{
+            let producto = stockProductos.find(item => item.id == boton.id)
+            console.log(`agregaste al carrito ${producto.nombre}`)
+            carrito.push(producto)
+            precioFinal += producto.precio
+        })
+    }
+})
+
+btncarrito.addEventListener('click', ()=>{
+    
+    console.log(carrito)
+    mostrarCarrito()
+    console.log(`el precio total del carrito es de $${precioFinal}`)
+})
+
+
+
+
+/* mostrarProductos(); */
+
+
+/* for (const boton of botonAgr){
+    console.log(boton)
+    boton.addEventListener('click', ()=>{
+        console.log(boton.id);
+        let producto = stockProductos.find(item => item.id == boton.id)
+    })
+} */
+
+/* console.log(botonAgr) */
+
+
+/* talles.addEventListener('change', ()=> {
+    console.log(talles.value)
+    if(talles.value =='all'){
+        mostrarProductos(stockProductos)
+        for (const boton of botonAgr){
+            boton.addEventListener('click', ()=>{
+                console.log(boton.id);
+                let producto = stockProductos.find(item => item.id == boton.id)
+            })
+        }
+    }else{
+        mostrarProductos(stockProductos.filter(el => el.talle == talles.value))
+        for (const boton of botonAgr){
+            boton.addEventListener('click', ()=>{
+                console.log(boton.id);
+                let producto = stockProductos.find(item => item.id == boton.id)
+            })
+        }
+    }
+}) */
+
+/* contenedor.innerHTML=''; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let cantidad = 0
 let carrito = ""
 let continuar = ""
 let precioFinal = 0
@@ -119,3 +265,4 @@ do{
     }
     continuar = prompt("¿Quieres hacer algo mas? Si, no.").toLowerCase()
 } while (continuar == "si")
+ */
